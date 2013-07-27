432f452462178974cd29f50495833bc3a7f5eeab
EC2 Default User <ec2-user@ip-10-112-91-21.ec2.internal>
2013-07-27 11:36:52 +0000

    pushing updated cron job file

--- populator.sh
+++ populator.sh
@@ -1,12 +1,19 @@
 #!/bin/sh
 
-cd /home/arjun/Desktop/GoOne
+cd /home/ec2-user/GoOne/GoOne
 PATH=$PATH:/usr/local/bin
 export PATH
-echo "Starting to crawl"
-scrapy crawl Eventful -a city_ids=./GoOne/location.list -t json -o  /home/arjun/Desktop/events.json
-echo "Importing to mongoDb"
-mongoimport --db events --collection events --type json --file  /home/arjun/Desktop/events.json --jsonArray
-rm  /home/arjun/Desktop/events.json
+echo "Starting to crawl eventful"
+scrapy crawl Eventful -a city_ids=./lists/eventful.list  -t json -o /home/ec2-user/GoOne/GoOne/eventful_events.json
+echo "Importing eventful to mongoDb"
+mongoimport --db events --collection events --type json --file  /home/ec2-user/GoOne/GoOne/eventful_events.json --jsonArray
+rm  /home/ec2-user/GoOne/GoOne/eventful_events.json
+
+echo "Starting to crawl eventbrite"
+shovel evenbrite_api_data_puller.eventbrite_extractor lists/eventbrite.list
+echo "Importing eventbrite to mongoDb"
+mongoimport --db events --collection events --type json --file  /home/ec2-user/GoOne/GoOne/eventbrite_events.json --jsonArray
+rm  /home/ec2-user/GoOne/GoOne/eventbrite_events.json
+
 
 
